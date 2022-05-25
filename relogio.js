6d7a0018c5d58181322c665cb5d0343d52765da4
Lazaro Martins <lazaromartins012345@gmail.com>
2022-05-25 19:47:05 -0300

    horas teste

---
+++ relogio.js
@@ -0,0 +1,17 @@
+/*function hors() {
+    let hor= document.getElementById('phoras')
+    let tela= document.getElementById('horas')
+    let horario= new Date()
+    let limpar= []
+    horario.getHours()
+    tela.innerHTML+= `${horario}`
+        console.log(horario)
+    
+}*/
+
+let hor= document.getElementById('phoras')
+let tela= document.getElementById('horas')
+    let horario= new Date()
+    let limpar= []
+    horario.getHours()
+    tela.innerHTML+= `${horario}`
